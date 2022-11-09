f28b209c8310d5729d120537ca264a9affabf841
Dhairya Gupta <dhairyag23@gmail.com>
2022-11-09 13:36:56 -0600

    WORKING WITH SERVER SIDE REDIRECTS!!

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,18 +1,25 @@
 import { useRouter } from "next/router";
 import fetch from "node-fetch";
+import { server } from "../config/index.js";
 
 const Post = () => {
-  const router = useRouter();
-  const { slug } = router.query;
-  var url;
-  fetch(`/api/${slug}`).then((res) =>
-    res.json().then((data) => {
-      url = data.url;
-      router.replace(url);
-    })
-  );
-
   return <div>Redirecting you...</div>;
 };
+
+async function getRedirect(slug) {
+  var fetchReq = await fetch(`${server}/api/${slug}`);
+  var data = await fetchReq.json();
+  return data;
+}
+
+export async function getServerSideProps(context) {
+  const { url } = await getRedirect(context.params.slug);
+  return {
+    redirect: {
+      permanent: false,
+      destination: `${url}`,
+    },
+  };
+}
 
 export default Post;
