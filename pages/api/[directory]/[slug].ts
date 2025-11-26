97b373233a39345dd5b8199eb412d5be39901947
Dhairya Gupta <dhairyag23@gmail.com>
2025-11-26 14:06:47 -0600

    bang?

--- pages/api/[directory]/[slug].ts
+++ pages/api/[directory]/[slug].ts
@@ -1,21 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { doc, getDoc } from 'firebase/firestore'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import firestore from '../../../bin/firebase'
+import { doc, getDoc } from "firebase/firestore";
+import type { NextApiRequest, NextApiResponse } from "next";
+import firestore from "../../../bin/firebase";
 type Data = {
-  url: string
-}
+  url: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { slug, directory } = req.query
-  const docRef =  doc(firestore, `${directory}`, `${slug}`);
+  const { slug, directory } = req.query;
+  const docRef = doc(firestore, `${directory}`, `${slug}`);
   var docData = await getDoc(docRef);
   if (docData.exists()) {
-    res.status(200).json({ url: docData.data().url })
+    res.status(200).json({ url: docData.data().url });
+    var newData = docData.data();
+
+    // how many times it was used
+    if (newData.count) {
+      newData.count += 1;
+    } else {
+      newData.count = 0;
+    }
   } else {
-  res.status(200).json({ url: '/' })
+    res.status(200).json({ url: "/" });
   }
 }
